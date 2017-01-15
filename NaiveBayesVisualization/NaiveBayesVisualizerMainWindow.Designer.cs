2322fbf33ffd7890d61ff63ab492578adae4651f
Arasz <rafal.araszkiewicz@student.put.poznan.pl>
2017-01-15 15:23:20 +0100

    Initialize repository

---
+++ NaiveBayesVisualization/NaiveBayesVisualizerMainWindow.Designer.cs
@@ -0,0 +1,51 @@
+﻿namespace NaiveBayesVisualization
+{
+    partial class NaiveBayesVisualizerMainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(NaiveBayesVisualizerMainWindow));
+            this.SuspendLayout();
+            // 
+            // NaiveBayesVisualizerMainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "NaiveBayesVisualizerMainWindow";
+            this.Text = "NaiveBayesVisualizer";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+

b5bd5b96d895becb66d010a69ef7beae644cd647
Rafał Araszkiewicz <rafal.araszkiewicz@student.put.poznan.pl>
2017-01-15 17:36:37 +0100

    Add data loading to grid

--- NaiveBayesVisualization/NaiveBayesVisualizerMainWindow.Designer.cs
+++ NaiveBayesVisualization/NaiveBayesVisualizerMainWindow.Designer.cs
@@ -29,20 +29,42 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(NaiveBayesVisualizerMainWindow));
-            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStrip = new System.Windows.Forms.ToolStrip();
+            this.loadDataButton = new System.Windows.Forms.ToolStripButton();
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
             this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.openDataFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.learningDataGridView = new System.Windows.Forms.DataGridView();
+            this.toolStrip.SuspendLayout();
             this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.learningDataGridView)).BeginInit();
             this.SuspendLayout();
             // 
-            // toolStrip1
+            // toolStrip
             // 
-            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
-            this.toolStrip1.Name = "toolStrip1";
-            this.toolStrip1.Size = new System.Drawing.Size(902, 25);
-            this.toolStrip1.TabIndex = 0;
-            this.toolStrip1.Text = "toolStrip1";
+            this.toolStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.loadDataButton});
+            this.toolStrip.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip.Name = "toolStrip";
+            this.toolStrip.Size = new System.Drawing.Size(902, 25);
+            this.toolStrip.TabIndex = 0;
+            this.toolStrip.Text = "Tool strip";
+            // 
+            // loadDataButton
+            // 
+            this.loadDataButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.loadDataButton.Image = global::NaiveBayesVisualization.Properties.Resources._1484516837_BT_folder_file_open;
+            this.loadDataButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.loadDataButton.Name = "loadDataButton";
+            this.loadDataButton.Size = new System.Drawing.Size(23, 22);
+            this.loadDataButton.Text = "toolStripButton1";
+            this.loadDataButton.Click += new System.EventHandler(this.LoadDataFromFile);
             // 
             // tabControl1
             // 
@@ -58,6 +80,7 @@
             // 
             // tabPage1
             // 
+            this.tabPage1.Controls.Add(this.splitContainer1);
             this.tabPage1.Location = new System.Drawing.Point(4, 22);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
@@ -71,10 +94,39 @@
             this.tabPage2.Location = new System.Drawing.Point(4, 22);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(894, 404);
+            this.tabPage2.Size = new System.Drawing.Size(894, 407);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "tabPage2";
             this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // openDataFileDialog
+            // 
+            this.openDataFileDialog.AddExtension = false;
+            this.openDataFileDialog.FileName = "D:\\Library\\Documents\\GitHub\\Active\\naive-bayes-visualization\\Data\\Mushrooms";
+            this.openDataFileDialog.Filter = "CSV files|*.csv";
+            this.openDataFileDialog.InitialDirectory = "../../Data/";
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(3, 3);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.learningDataGridView);
+            this.splitContainer1.Size = new System.Drawing.Size(888, 401);
+            this.splitContainer1.SplitterDistance = 422;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // learningDataGridView
+            // 
+            this.learningDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.learningDataGridView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.learningDataGridView.Location = new System.Drawing.Point(0, 0);
+            this.learningDataGridView.Name = "learningDataGridView";
+            this.learningDataGridView.Size = new System.Drawing.Size(422, 401);
+            this.learningDataGridView.TabIndex = 0;
             // 
             // NaiveBayesVisualizerMainWindow
             // 
@@ -84,11 +136,18 @@
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(902, 458);
             this.Controls.Add(this.tabControl1);
-            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.toolStrip);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "NaiveBayesVisualizerMainWindow";
             this.Text = "NaiveBayesVisualizer";
+            this.toolStrip.ResumeLayout(false);
+            this.toolStrip.PerformLayout();
             this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.learningDataGridView)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -96,10 +155,14 @@
 
         #endregion
 
-        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStrip toolStrip;
         private System.Windows.Forms.TabControl tabControl1;
         private System.Windows.Forms.TabPage tabPage1;
         private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.ToolStripButton loadDataButton;
+        private System.Windows.Forms.OpenFileDialog openDataFileDialog;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.DataGridView learningDataGridView;
     }
 }
 
--- NaiveBayesVisualization/NaiveBayesVisualizerMainWindow.cs
+++ NaiveBayesVisualization/NaiveBayesVisualizerMainWindow.cs
@@ -7,14 +7,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using NaiveBayesVisualization.Data;
 
 namespace NaiveBayesVisualization
 {
     public partial class NaiveBayesVisualizerMainWindow : Form
     {
+        private ExperimentData _experimentData;
+
         public NaiveBayesVisualizerMainWindow()
         {
             InitializeComponent();
+        }
+
+        private void LoadDataFromFile(object sender, EventArgs e)
+        {
+            var result = openDataFileDialog.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                var dataReader = new CsvDataReader(openDataFileDialog.FileName);
+                _experimentData = new ExperimentData(dataReader);
+                _experimentData.Load();
+
+                learningDataGridView.DataSource = _experimentData.TrainData;
+            }
         }
     }
 }

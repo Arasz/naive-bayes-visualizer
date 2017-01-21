f2e4ca9b412fc19d84b2f41948eb86725f2d05e1
Rafał Araszkiewicz <rafal.araszkiewicz@student.put.poznan.pl>
2017-01-21 12:39:49 +0100

    Add datga code dir

---
+++ NaiveBayesVisualization/Data/CsvDataReader.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using Accord.IO;
+
+namespace NaiveBayesVisualization.Data
+{
+    public class CsvDataReader
+    {
+        private readonly string _dataPath;
+        private readonly bool _hasHeaders;
+
+        public CsvDataReader(string dataPath, bool hasHeaders = true)
+        {
+            _dataPath = dataPath;
+            _hasHeaders = hasHeaders;
+        }
+
+        public string[] Headers { get; private set; }
+
+        private CsvReader NewReader => new CsvReader(_dataPath, _hasHeaders);
+
+        public IList<string[]> ReadLines()
+        {
+            using (var reader = NewReader)
+            {
+                Headers = reader.GetFieldHeaders();
+                return reader.ReadToEnd();
+            }
+        }
+    }
+}
